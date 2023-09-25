b7520ea2f65d50aab106f9d5cbe950544695a812
Merdan <merdan@Merdans-MacBook-Air.local>
2023-09-25 10:58:31 +0200

    „Fertig“

---
+++ BankTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace BankTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ KontoTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Bank;
+namespace KontoTest;
+
+[TestClass]
+public class KontoTests
+{
+    [TestMethod]
+    public void Konto_GuthabenEinzahlen()
+    {
+        //Arrange
+        int startGuthaben = 100;
+        Konto testKonto = new Konto(startGuthaben);
+
+        int eingezahlterBetrag = 50;
+        //Act
+        testKonto.Einzahlen(eingezahlterBetrag);
+
+        //Assert
+        Assert.AreEqual(150,testKonto.Guthaben);
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Konto_GuthabenAuszahlen()
+    {
+        //Arrange
+        Konto testKonto = new Konto(150);
+
+        //Act
+        testKonto.Auszahlen(1000);
+
+        //Assert
+    }
+}
